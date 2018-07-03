8ccf389eef7c23620dba467ceea14ac516711d96
Denis <33689745+notorious16@users.noreply.github.com>
2018-07-03 15:28:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,21 +3,33 @@ package net.ukr.kurach.d;
 import java.util.Scanner;
 
 /**
- * In the class was implemented the computation of the circumference.
+ * Написать программу которая считает 4 числа c клавиатуры и выведет на экран
+ * самое большое из них.
  * 
- * @version 1.0 01 July 2018
- * @author Denis Kurach
- *
  */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input radius");
-		double r = sc.nextDouble();
-		double c = 2 * Math.PI * r;
-		System.out.println("Circumference is "+c);
+		System.out.println("Input first number");
+		int a = sc.nextInt();
+		System.out.println("Input second number");
+		int b = sc.nextInt();
+		System.out.println("Input third number");
+		int c = sc.nextInt();
+		System.out.println("Input forth number");
+		int d = sc.nextInt();
+
+		if (a > b && a > c && a > d) {
+			System.out.println("First number: (" + a + ") is the biggest one");
+		} else if (b > a && b > c && b > d) {
+			System.out.println("Second number: (" + b + ") is the biggest one");
+		} else if (c > a && c > b && c > d) {
+			System.out.println("Third number: (" + c + ") is the biggest one");
+		} else if (d > a && d > b && d > c) {
+			System.out.println("Forth number (" + d + ") is the biggest one");
+		}
 		sc.close();
 	}
 
