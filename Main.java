30273d72f808ddcf40ab9cac2c15525bbb84e23b
Denis <33689745+notorious16@users.noreply.github.com>
2018-07-03 15:30:37 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,31 +1,25 @@
 package net.ukr.kurach.d;
 
-import java.util.Scanner;
-
 /**
+ * 
+ * @author Денис
  *
- *Определить количество дней в году, который вводит пользователь. 
- *В високосном годе - 366 дней, тогда как в обычном 365. 
- *Високосный год определяется по следующему правилу:  
- *Год високосный, если он делится на четыре без остатка, 
- *но если он делится на 100 без остатка, 
- *это не високосный год. 
- *Однако, если он делится без остатка на 400, это високосный год. 
- *
+ *         Треугольник существует только тогда, когда сумма любых двух его
+ *         сторон больше третьей. Дано: a, b, c – стороны предполагаемого
+ *         треугольника. Напишите программу которая укажет существует такой
+ *         треугольник или нет.
  */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		System.out.println("input year");
-		int year = sc.nextInt();
-		double a = (4.5 % 4);
-		System.out.println(a);
-		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-			System.out.println("366");
-		}else {
-			System.out.println("365");
+		int a = 2;
+		int b = 3;
+		int c = 4;
+		if (a + b > c && b + c > a && a + c > b) {
+			System.out.println("Треугольник существует");
+		} else {
+			System.out.println("Треугольник не существует");
 		}
 	}
 
