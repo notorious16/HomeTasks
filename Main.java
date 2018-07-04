6d60343efc3d836b67925288079bbdd673093b5d
Denis <33689745+notorious16@users.noreply.github.com>
2018-07-04 17:37:31 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -18,7 +18,7 @@ public class Main {
 		int w = sc.nextInt();
 		for (int i = 1; i <= h; i++) {
 			for (int j = 1; j <= w; j++) {
-				if (j == 1 || j == w || i == 1 || i % h == 0) {
+				if (j == 1 || j == w || i == 1 || i == h) {
 					System.out.print("*");
 				} else {
 					System.out.print(" ");
