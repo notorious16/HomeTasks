d800ccd20a96daa11c5f10c415f9cafef8274844
Denis <33689745+notorious16@users.noreply.github.com>
2018-07-04 16:08:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,30 @@
 package net.ukr.kurach.d;
 
+import java.util.Scanner;
+
 /**
  * 
- * Напечатайте таблицу умножения на 5. предпочтительно печатать 1 x 5 = 5, 2 x 5
- * = 10, а не просто 5, 10 и т. д.
+ * Выведите на экран прямоугольник из *. Причем высота и ширина прямоугольника
+ * вводятся с клавиатуры.
  *
  */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int h = 1;
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input height");
+		int h = sc.nextInt();
+		System.out.println("Input width");
+		int w = sc.nextInt();
 		for (int i = 1; i <= h; i++) {
-			for (int j = 1; j <= 10; j++) {
-				int a = 5;
-				int b = j * a;
-				System.out.print(j + " x " + a + " = " + b);
-				System.out.println();
+			for (int j = 1; j <= w; j++) {
+				if (j == 1 || j == w || i == 1 || i % h == 0) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
 			}
+			System.out.println();
 		}
 	}
 
